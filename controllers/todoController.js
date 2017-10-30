bd65783207d7040dbc3299af269380117c1b6fbb
Bishnu Rana <bishnu.rana2218@gmail.com>
2017-10-30 17:40:59 +0200

    stuck with db auth error

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,44 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+//connect to the database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://<user>:<user>@ds139187.mlab.com:39187/todone');
+
+//create schema
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+
+//create model
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemFirst = Todo({item:'Run 10km'}).save(function(err){
+	if (err) throw err;
+	console.log('Item sucessfully saved');
+});
+
+// body parser middleware
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var data=[{item:'buy food'},{item:'play xbox'},{item:'milk cow'}];
 module.exports = function (app) {
 
 	app.get('/todo', function(req, res){
-		res.render('todo');
+		res.render('todo',{todos:data});
 	});
 
-	app.post('/todo', function(req, res){
+	app.post('/todo', urlencodedParser, function(req, res){
 
+		data.push(req.body);
+		res.json(data);
 	});
 
-	app.delete('/todo', function(req, res){
-
+	app.delete('/todo/:item', function(req, res){
+		data = data.filter(function(todo){
+			return todo.item.replace(/ /g,"-") !== req.params.item;
+		});
+		res.json(data);
 	});
 
 
