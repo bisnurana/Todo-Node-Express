b6fbb15f02a48d9450664eb143da46a371d75c4e
Bishnu Rana <bishnu.rana2218@gmail.com>
2017-10-31 13:31:54 +0200

    connected to local db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,20 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 //connect to the database
+var uri = 'mongodb://localhost:27017/todos';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<user>:<user>@ds139187.mlab.com:39187/todone');
+mongoose.connect(uri);
+var db = mongoose.connection;
+
+//check connection
+db.once('open', function(){
+  console.log('Connected to mongo db');
+});
+
+//check db for error
+db.on('error', function(err){
+  console.log(err);
+});
 
 //create schema
 var todoSchema = new mongoose.Schema({
@@ -12,33 +24,43 @@ var todoSchema = new mongoose.Schema({
 //create model
 var Todo = mongoose.model('Todo', todoSchema);
 
+/*Dummy data
 var itemFirst = Todo({item:'Run 10km'}).save(function(err){
-	if (err) throw err;
-	console.log('Item sucessfully saved');
-});
+	if(err) throw err;
+	console.log('Sucessfully item added');
+});*/
 
 // body parser middleware
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var data=[{item:'buy food'},{item:'play xbox'},{item:'milk cow'}];
+//var data=[{item:'buy food'},{item:'play xbox'},{item:'milk cow'}];
 module.exports = function (app) {
 
 	app.get('/todo', function(req, res){
-		res.render('todo',{todos:data});
+		Todo.find({}, function(err,data){
+			if (err) throw err;
+			res.render('todo',{todos:data});
+		});
+		
 	});
 
 	app.post('/todo', urlencodedParser, function(req, res){
+		var newTodo = Todo(req.body).save(function(err,data){
+			if (err) throw err;
+			res.json(data);
 
-		data.push(req.body);
-		res.json(data);
+		});
+		
 	});
 
 	app.delete('/todo/:item', function(req, res){
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g,"-") !== req.params.item;
+		Todo.find({item:req.params.item.replace(/\-/g, ' ')}).remove(function(err, data){
+			if(err) throw err;
+			res.json(data);
+
 		});
-		res.json(data);
+		
 	});
 
 
